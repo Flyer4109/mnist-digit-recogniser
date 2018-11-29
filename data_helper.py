197b8da7009a431c24d93bfc708e88f8f11c7022
Flyer4109 <isao@rayamos.com>
2018-11-29 18:56:53 +0000

    change targets to binary matrices

--- data_helper.py
+++ data_helper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from keras.utils import to_categorical
 
 
 # function used to load data and returns appropriate numpy arrays
@@ -25,17 +26,20 @@ def load_data(k=10):
     training_data = scaler.transform(training_data)
     testing_data = scaler.transform(testing_data)
 
-    # reshape train data to (num_train_data, 784)
+    # reshape train and test data to (num_data, 784)
     training_data = np.reshape(training_data, (len(data_set.index)-cut_index, 784))
-
-    # gets train targets in numpy array in shape (num_train_targets)
-    training_targets = data_set['label'][:-cut_index]
-
-    # reshape test data to (num_test_data, 784)
     testing_data = np.reshape(testing_data, (cut_index, 784))
 
-    # gets test targets in numpy array in shape (num_test_targets)
-    testing_targets = data_set['label'][-cut_index:]
+    # gets all the labels in numpy array in shape (42000,)
+    targets = data_set['label']
+
+    # cut targets into training and testing parts using cut_index
+    training_targets = targets[:-cut_index]
+    testing_targets = targets[-cut_index:]
+
+    # gets train and test targets in numpy array in shape (num_targets, 10)
+    training_targets = to_categorical(training_targets, num_classes=10)
+    testing_targets = to_categorical(testing_targets, num_classes=10)
 
     # returns everything needed for training and testing in 2 tuples
     return (training_data, training_targets), (testing_data, testing_targets)
